5840e983b0712709d25495c87d74c50ddd24cf60
Alexander Korolev <kilork@yandex.ru>
2020-05-23 20:19:26 +0200

    use typescript compiler plugin for css modules

--- src/styles.module.css.d.ts
+++
@@ -1,6 +0,0 @@
-export const timeline: string;
-export const serie: string;
-export const label: string;
-export const left: string;
-export const right: string;
-export const editor: string;
--- src/timeline.ts
+++ src/timeline.ts
@@ -1,4 +1,4 @@
-import * as styles from "./styles.module.css";
+import styles from "./styles.module.css";
 import { TimelineSerie } from "./timeline_serie";
 
 export class Timeline {
